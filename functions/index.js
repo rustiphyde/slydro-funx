bb062a1449e7522d767d666dae56a7513ec9cd05
Rusty Hoppins <rustiphyde@gmail.com>
2021-03-02 23:11:09 -0600

    Initial Commit with dependencies and api function

---
+++ functions/index.js
@@ -0,0 +1,12 @@
+const functions = require("firebase-functions");
+
+const app = require("express")();
+
+const FBAuth = require("./util/fbAuth");
+
+const cors = require("cors");
+app.use(cors());
+
+const { db, admin } = require("./util/admin");
+
+exports.api = functions.https.onRequest(app);
